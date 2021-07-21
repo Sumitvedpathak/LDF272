7a72acf4a1b4783752ef7fc14215a82e3dd0a1df
Sumit Vedpathak <sumitvedpathak@gmail.com>
2021-07-21 07:55:06 -0400

    updates

--- chaincodes/chaincode_interaction/lib/interact.js
+++ chaincodes/chaincode_interaction/lib/interact.js
@@ -6,7 +6,8 @@ class InteractContract extends Contract{
 
     async interact(ctx, channel, chaincode, argsStr){
         const args = JSON.parse(argsStr);
-        const ccresponse = ctx.stub.invokeChaincode(chaincode, args, channel);
+        console.log("Arguments - "+JSON.stringify(args));
+        const ccresponse = await ctx.stub.invokeChaincode(chaincode, args, channel);
         if(ccresponse.status >= 400){
             throw new Error(`Chaincode ${chaincode} from channel ${channel} returned ${ccresponse.status} status code: ${ccresponse.message}`);
         }
